2fabfee7f91253341326aa40012e96ccfb1f2a0c
Walter462 <74890413+Walter462@users.noreply.github.com>
2024-07-25 13:48:01 +0800

    added requirements

--- LAUNCH.py
+++ LAUNCH.py
@@ -2,3 +2,12 @@
 from viz_engine_app import app
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+import subprocess
+    
+# Use pip freeze to get the list of installed packages
+requirements = subprocess.run(['pip', 'freeze'], capture_output=True, text=True)
+
+# Write the output to a requirements.txt file
+with open('requirements.txt', 'w') as f:
+    f.write(requirements.stdout)
